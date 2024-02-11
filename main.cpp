06e15848c0ca9928f721f81cb652b44eea9206c4
exFull <vladbelk321@gmail.com>
2024-02-11 17:34:57 +0200

    up lab

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<cmath>
+#include<conio.h>
 using namespace std;
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+int main()
+{
+	double y, x;
+	cout << "Enter x: ";
+	cin >> x;
+
+	y = pow(sin(x), 2) + 4 * cos(x);
+	cout << "y = " << y << endl;
+	_getch();
 	system("pause");
 	return 0;
 }
+
