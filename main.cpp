55924a80bfc087c2b60927658d7dc432217b3f4d
exFull <vladbelk321@gmail.com>
2024-02-16 09:43:58 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <windows.h>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
+	cout << "\033[3mKraj lukomor'a dub zelenyj," << endl;
+	Beep(750, 300);
+
+	cout << "\033[3mI zolotyj lancyuh na nim:" << endl;
+	Beep(750, 300);
+
+	cout << "\033[3mShodnya, shonochi kit uchenyj" << endl;
+	Beep(750, 300);
+
+	cout << "\033[3mNa lancyuhu kruzhl'aje tim" << endl;
+
 	return 0;
 }
