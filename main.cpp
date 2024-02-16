d7c1eb19eff92d1200feb99d969b606d84afa993
exFull <vladbelk321@gmail.com>
2024-02-16 09:20:45 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
+
+	double v;
+	cout << "Enter the initial velocity: ";
+	cin >> v;
+
+	double t;
+	cout << "Enter the time: ";
+	cin >> t;
+
+	double a;
+	cout << "Enter Acceleration: ";
+	cin >> a;
+
+	double S = v * t + (a * t * t) / 2;
+
+	cout << "Distance traveled: " << S << endl;
+
 	return 0;
 }
