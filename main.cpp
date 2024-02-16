63624949c7abe16b94edca79a05f8a982af1141c
exFull <vladbelk321@gmail.com>
2024-02-16 09:16:03 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
+	double L;
+	cout << "Enter the circumference: ";
+	cin >> L;
+
+	double pi = 3.141592653589793;
+	double R = L / (2 * pi);
+
+	double S = pi * R * R;
+
+	cout << "Circumference: " << S << endl;
+
 	return 0;
 }
