2047028c1f900c0b3f21ff877eb425c5727d4d42
exFull <vladbelk321@gmail.com>
2024-02-18 16:06:12 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int days;
+
+    cout << "Enter the number of days: ";
+    cin >> days;
+
+    if (days < 0) {
+        cout << "Error: Please enter a non-negative number of days." << endl;
+        return 1;
+    }
+
+    int weeks = (days + 6) / 7;
+    int remainingDays = days % 7;
+
+    cout << days << " days is equal to " << weeks << " weeks and " << remainingDays << " days." << endl;
+
+    return 0;
 }
