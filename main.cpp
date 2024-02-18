a7f7b522619e15d8a8a844b99721ab32fed63978
exFull <vladbelk321@gmail.com>
2024-02-18 17:56:26 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    double radius;
+
+    cout << "Enter the radius of the sphere (cm): ";
+    cin >> radius;
+
+    if (radius <= 0) {
+        cerr << "Error: Radius must be a positive number." << endl;
+        return 1;
+    }
+
+    const double pi = 3.14159;
+    double volume = 4.0 / 3.0 * pi * pow(radius, 3);
+
+    cout << "The volume of the sphere is: " << volume << " cubic centimeters." << endl;
+
+    return 0;
 }
