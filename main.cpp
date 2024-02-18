b43cee0c2378a7b3a5f40e7ed36d7fc83da02e5b
exFull <vladbelk321@gmail.com>
2024-02-18 17:01:26 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    cout << "\n";
+
+    // Верхня межа
+    cout << (char)201;
+    for (int i = 0; i < 55; i++) {
+        cout << (char)205;
+    }
+    cout << (char)187 << endl;
+
+    // Заголовок
+    cout << (char)186 << "                        Pory Roku                      " << (char)186 << endl;
+
+    // Середня межа
+    cout << (char)204;
+    for (int i = 0; i < 55; i++) {
+        cout << (char)205;
+    }
+    cout << (char)185 << endl;
+
+    // Рядки
+    cout << (char)186 << "     Zyma     " << (char)186 << "    Vesna     " << (char)186 << "    Lito     " << (char)186 << "    Osin   " << (char)186 << endl;
+
+    // Нижня межа
+    cout << (char)200;
+    for (int i = 0; i < 55; i++) {
+        cout << (char)205;
+    }
+    cout << (char)188 << endl;
+
+    return 0;
 }
+
