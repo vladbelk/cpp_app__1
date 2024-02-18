d926f4b62548f01872a4a277bdd1aa0264b99d09
exFull <vladbelk321@gmail.com>
2024-02-18 17:23:55 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    double length, width, height;
+
+    std::cout << "Enter the length of the parallelepiped (in centimeter): ";
+    std::cin >> length;
+    std::cout << "Enter the width of the parallelepiped (in centimeter): ";
+    std::cin >> width;
+    std::cout << "Enter the height of the parallelepiped (in centimeter): ";
+    std::cin >> height;
+
+    double volume = length * width * height;
+
+    std::cout << "The volume of the parallelepiped is: " << volume << " cubic meters\n";
+
+    return 0;
 }
