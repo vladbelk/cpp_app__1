ce0051d585431ebe01e94d2e198eebcff6d62b2e
exFull <vladbelk321@gmail.com>
2024-02-23 15:53:14 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int inputNumber;
+    cout << "Enter a four-digit number: ";
+    cin >> inputNumber;
+
+    if (inputNumber < 1000 || inputNumber > 9999) {
+        cout << "Error: Please enter a valid four-digit number." << endl;
+        return 1;
+    }
+
+    int digit1 = inputNumber / 1000;
+    int digit2 = (inputNumber / 100) % 10;
+    int digit3 = (inputNumber / 10) % 10;
+    int digit4 = inputNumber % 10;
+
+    int newNumber = digit2 * 1000 + digit1 * 100 + digit4 * 10 + digit3;
+
+    cout << "Original number: " << inputNumber << endl;
+    cout << "Modified number: " << newNumber << endl;
+
+    return 0;
 }
