4a694879928958afdef6ae82d63cba0fd8281178
exFull <vladbelk321@gmail.com>
2024-02-23 16:22:47 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    double inputNumber;
+    cout << "Enter a number: ";
+    cin >> inputNumber;
+
+    if (fmod(inputNumber, 2) == 0) {
+        double result = inputNumber * 3;
+        cout << "The number is even. Result after multiplying by 3: " << result << endl;
+    }
+    else {
+        double result = inputNumber / 2;
+        cout << "The number is odd. Result after dividing by 2: " << result << endl;
+    }
+
+    return 0;
 }
