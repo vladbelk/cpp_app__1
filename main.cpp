055bcf50a145a3245f34315a22f92c18ca02da71
exFull <vladbelk321@gmail.com>
2024-02-23 16:08:30 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
+	double num1, num2;
+
+	cout << "Enter the first number: ";
+	cin >> num1;
+
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	double smaller_num = (num1 < num2) ? num1 : num2;
+
+	cout << "The smaller number is: " << smaller_num << endl;
+
 	return 0;
 }
