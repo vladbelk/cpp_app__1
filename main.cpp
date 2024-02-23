3703480f07124649268c5025311d35c32a93cb54
exFull <vladbelk321@gmail.com>
2024-02-23 15:55:23 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int max_num, num;
+
+    cout << "Enter the first number: ";
+    cin >> max_num;
+
+    for (int i = 1; i < 7; ++i) {
+        cout << "Enter the next number: ";
+        cin >> num;
+
+        if (num > max_num) {
+            max_num = num;
+        }
+    }
+
+    cout << "The maximum number is: " << max_num << endl;
+
+    return 0;
 }
