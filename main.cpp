a6b3a23dd25c6b9b80d549cc6907d55fa2782ed3
exFull <vladbelk321@gmail.com>
2024-02-23 16:25:10 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    double num1, num2;
+    char operation;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Select an operation (+, -, *, /): ";
+    cin >> operation;
+
+    if (operation != '+' && operation != '-' && operation != '*' && operation != '/') {
+        cout << "Invalid operation!" << endl;
+        return 1;
+    }
+
+    double result;
+    switch (operation) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 == 0) {
+            cout << "Error: Division by zero!" << endl;
+            return 1;
+        }
+        result = num1 / num2;
+        break;
+    default:
+        cout << "Unexpected error!" << endl;
+        return 1;
+    }
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
 }
