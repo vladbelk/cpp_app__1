420cd55ce656da00791d1a81688ddc28b7182b71
exFull <vladbelk321@gmail.com>
2024-02-25 16:45:17 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int* ptr1 = &num1;
+    int* ptr2 = &num2;
+
+    if (*ptr1 > *ptr2) {
+        cout << "The greater number is: " << *ptr1 << endl;
+    }
+    else if (*ptr1 < *ptr2) {
+        cout << "The greater number is: " << *ptr2 << endl;
+    }
+    else {
+        cout << "The numbers are equal." << endl;
+    }
+
+    return 0;
 }
