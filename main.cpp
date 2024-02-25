cf59b9badce16a4e82a5e7ca31d8ae2ff0c8ec4c
exFull <vladbelk321@gmail.com>
2024-02-25 17:03:59 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
+	int a = 10;
+	int b = 20;
+
+	int* ptr_a = &a;
+	int* ptr_b = &b;
+
+	int temp = *ptr_a;
+	*ptr_a = *ptr_b;
+	*ptr_b = temp;
+
+	std::cout << "The value of A after the change: " << a << std::endl;
+	std::cout << "The value of B after the change " << b << std::endl;
+
 	return 0;
 }
