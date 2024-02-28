95bcd10dd6f9f5391f0f60464a1495efe75646aa
exFull <vladbelk321@gmail.com>
2024-02-28 12:04:13 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
+void reverseCopyArray(int* source, int* destination, int size) {
+    int* ptrSource = source + size - 1;
+    int* ptrDestination = destination;
+
+    for (int i = 0; i < size; i++) {
+        *ptrDestination = *ptrSource;
+        ptrSource--;
+        ptrDestination++;
+    }
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int source[] = { 1, 2, 3, 4, 5 };
+    int destination[5];
+    int size = sizeof(source) / sizeof(source[0]);
+
+    reverseCopyArray(source, destination, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << destination[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
