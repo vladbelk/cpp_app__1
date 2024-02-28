22cc94c0c472e4be323c0217c91a069bf4589c49
exFull <vladbelk321@gmail.com>
2024-02-28 11:51:19 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    double num1, num2;
+    char operation;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    double result;
+
+    switch (operation) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0) {
+            result = num1 / num2;
+        }
+        else {
+            std::cout << "Error: Division by Zero!" << std::endl;
+            return 1;
+        }
+        break;
+    default:
+        std::cout << "Error: Unknown operation!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Outcome: " << result << std::endl;
+
+    return 0;
 }
