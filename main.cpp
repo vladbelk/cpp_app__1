f421cd086df377a67610b4a83fff25aadbba6f4e
exFull <vladbelk321@gmail.com>
2024-02-28 11:59:07 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
+void reverseArray(int* arr, int size) {
+    int* ptrStart = arr;
+    int* ptrEnd = arr + size - 1;
+
+    int temp;
+
+    while (ptrStart < ptrEnd) {
+        temp = *ptrStart;
+        *ptrStart = *ptrEnd;
+        *ptrEnd = temp;
+
+        ptrStart++;
+        ptrEnd--;
+    }
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
