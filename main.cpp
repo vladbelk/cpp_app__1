a19987159114b5c5508d521a34970e198ee161f0
exFull <vladbelk321@gmail.com>
2024-02-28 11:54:16 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int* ptr = arr;
+
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += *ptr;
+        ptr++;
+    }
+
+    cout << "Sum of array elements: " << sum << endl;
+
+    return 0;
 }
