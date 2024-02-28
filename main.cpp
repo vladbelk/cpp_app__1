2b75d344dd29c4c437a3d341752e571b02dbc05a
exFull <vladbelk321@gmail.com>
2024-02-28 11:57:42 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int source[] = { 1, 2, 3, 4, 5 };
+    int destination[5];
+    int size = sizeof(source) / sizeof(source[0]);
+
+    int* ptrSource = source;
+    int* ptrDestination = destination;
+
+    for (int i = 0; i < size; i++) {
+        *ptrDestination = *ptrSource;
+        ptrSource++;
+        ptrDestination++;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << destination[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
