76600aa07f0462025a76c2f7f943b621e0e895c4
exFull <vladbelk321@gmail.com>
2024-03-03 17:44:38 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    const int rows = 3;
+    const int cols = 4;
+    int matrix[rows][cols] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int sum = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            sum += matrix[i][j];
+        }
+    }
+    cout << "Sum of all elements of the array: " << sum << endl;
+
+    float average = static_cast<float>(sum) / (rows * cols);
+    cout << "The arithmetic mean of all elements of the array: " << average << endl;
+
+    int minElement = matrix[0][0];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (matrix[i][j] < minElement) {
+                minElement = matrix[i][j];
+            }
+        }
+    }
+    cout << "Minimal array element: " << minElement << endl;
+
+    int maxElement = matrix[0][0];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (matrix[i][j] > maxElement) {
+                maxElement = matrix[i][j];
+            }
+        }
+    }
+    cout << "Maximum array element: " << maxElement << endl;
+
+    return 0;
 }
