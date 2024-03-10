b57c7ed45ee81d6a25544bcedfb6661606d158cd
exFull <vladbelk321@gmail.com>
2024-03-10 11:50:04 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
-#include<iostream>
+#include <iostream>
+#include <random>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int array1[5][10];
+    int array2[5][5];
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(0, 50);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+            array1[i][j] = dist(gen);
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j == 0) {
+                array2[i][j] = array1[i][0] + array1[i][1];
+            }
+            else {
+                array2[i][j] = array1[i][2 * j] + array1[i][2 * j + 1];
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << array1[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << array2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
