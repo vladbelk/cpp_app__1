2162beb32e0633359f1fadfbe7f28dced0e3b022
exFull <vladbelk321@gmail.com>
2024-03-10 11:54:35 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+const int ROWS = 3;
+const int COLS = 3;
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int userNumber;
+    cout << "Enter a number: ";
+    cin >> userNumber;
+
+    int myArray[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            if (i == 0) {
+                myArray[i][j] = userNumber;
+            }
+            else {
+                myArray[i][j] = myArray[i - 1][j] * 2;
+            }
+        }
+    }
+
+    cout << "Created array:" << endl;
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            cout << myArray[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
