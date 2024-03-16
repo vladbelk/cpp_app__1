99e7596d2bb0af862d46000bed6bfa8793c6c086
exFull <vladbelk321@gmail.com>
2024-03-16 11:38:04 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+
+int* removeNegatives(int* arr, int& size) {
+    int count = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] >= 0) {
+            count++;
+        }
+    }
+
+    int* newArr = new int[count];
+    int index = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] >= 0) {
+            newArr[index++] = arr[i];
+        }
+    }
+
+    size = count;
+
+    return newArr;
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int originalArray[] = { -3, 1, -5, 2, 8, -7, 6, -9 };
+    int size = sizeof(originalArray) / sizeof(originalArray[0]);
+
+    int* resultArray = removeNegatives(originalArray, size);
+
+    std::cout << "Array after removing negative numbers: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << resultArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] resultArray;
+
+    return 0;
 }
