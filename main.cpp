622de366ae663c0fb0e2772de422b2e8b1b2cd85
exFull <vladbelk321@gmail.com>
2024-03-16 11:03:12 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
+void sum_and_product(int* arr, int size, int* sum, int* product) {
+    *sum = 0;
+    *product = 1;
+    for (int i = 0; i < size; i++) {
+        *sum += arr[i];
+        *product *= arr[i];
+    }
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int sum, product;
+
+    sum_and_product(arr, size, &sum, &product);
+
+    cout << "Amount: " << sum << endl;
+    cout << "Product: " << product << endl;
+
+    return 0;
 }
