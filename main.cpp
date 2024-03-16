5e2be18c70a5e8ccf1afc92171911b9e8d865597
exFull <vladbelk321@gmail.com>
2024-03-16 11:41:55 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+
+void addElementsToEnd(int*& arr, int& size, const int* elements, int blockSize) {
+
+    int newSize = size + blockSize;
+    int* newArray = new int[newSize];
+
+    for (int i = 0; i < size; ++i) {
+        newArray[i] = arr[i];
+    }
+
+    for (int i = 0; i < blockSize; ++i) {
+        newArray[size + i] = elements[i];
+    }
+
+    delete[] arr;
+    arr = newArray;
+    size = newSize;
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    int size = 5;
+    int* arr = new int[size] {1, 2, 3, 4, 5};
+
+    std::cout << "Original array:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int newElements[] = { 6, 7, 8 };
+    int blockSize = sizeof(newElements) / sizeof(newElements[0]);
+
+    addElementsToEnd(arr, size, newElements, blockSize);
+
+    std::cout << "Array after adding new elements:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] arr;
+
+    return 0;
 }
