3e289af4740d9c0592e1c092c0e6b4665c2b606f
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:54:55 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
+string insertCharacterAtIndex(string str, int index, char ch) {
+    str.insert(index, 1, ch);
+    return str;
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    string str;
+    char ch;
+    int index;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Enter the character to insert: ";
+    cin >> ch;
+
+    cout << "Enter the index to insert the character: ";
+    cin >> index;
+
+    if (index >= 0 && index <= str.length()) {
+        string result = insertCharacterAtIndex(str, index, ch);
+        cout << "Resulting string: " << result << endl;
+    }
+    else {
+        cout << "Error: Invalid index." << endl;
+    }
+
+    return 0;
 }
