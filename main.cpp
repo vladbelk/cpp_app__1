6f2c3e7be15b60279ecb378fb219265114e07774
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:57:13 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
+int countOccurrences(const string& str, char target) {
+    int count = 0;
+    for (char ch : str) {
+        if (ch == target) {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    string str;
+    char target;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Enter the character to search for: ";
+    cin >> target;
+
+    int occurrences = countOccurrences(str, target);
+    cout << "The character '" << target << "' occurs " << occurrences << " times in the string." << endl;
+
+    return 0;
 }
