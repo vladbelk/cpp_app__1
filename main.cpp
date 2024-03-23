d7ba3f3c81daffb7b198ccff4f2dffe4e79e5d41
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:53:33 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
+string removeAllOccurrences(string str, char ch) {
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] == ch) {
+            str.erase(i, 1);
+            i--;
+        }
+    }
+    return str;
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    string str;
+    char ch;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Enter the character to remove: ";
+    cin >> ch;
+
+    string result = removeAllOccurrences(str, ch);
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
 }
