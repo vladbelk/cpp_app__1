a9de8efaca2512cd9a3f01cefd01445d6bee8d28
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:30:32 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <iterator>
+#include <regex>
+#include <string>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    std::string const text = "Quick brown fox";
+    std::string output;
+    std::regex const space(R"(\s)");
+    std::regex_replace(std::back_inserter(output), text.begin(), text.end(), space, "\t");
+    std::cout << '\n' << output << '\n';
 }
