18f22f7c92d8139eb79a7ff3a56024a421632612
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:42:24 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-#include<iostream>
+#include <iostream>
+#include <cctype>
+
 using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    string str = "This is a string with characters 1234 and special characters !@#$%^&*()";
+
+    int letters = 0, digits = 0, otherChars = 0;
+
+    for (char c : str) {
+        if (isalpha(c)) {
+            letters++;
+        }
+        else if (isdigit(c)) {
+            digits++;
+        }
+        else if (!isspace(c)) {
+            otherChars++;
+        }
+    }
+
+    cout << "Number of letters: " << letters << endl;
+    cout << "Number of digits: " << digits << endl;
+    cout << "Number of other characters: " << otherChars << endl;
+
+    return 0;
 }
