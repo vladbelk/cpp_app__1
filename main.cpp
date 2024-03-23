af20fb91afd81ebeaab84bcbb878d93a59b9c04d
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:49:40 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
+string removeCharacterAtIndex(string str, int index) {
+    if (index >= 0 && index < str.length()) {
+        str.erase(index, 1);
+        return str;
+    }
+    else {
+        cout << "Error: Invalid index." << endl;
+        return str;
+    }
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    string str;
+    int index;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Enter the index of the character to remove: ";
+    cin >> index;
+
+    string result = removeCharacterAtIndex(str, index);
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
 }
