5eb0d4b8b3889f801eb3b1dfaa09ea85d2e16245
vladbelk <vladbelk321@gmail.com>
2024-03-23 11:56:08 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
+string replaceDotsWithExclamation(string str) {
+    for (char& c : str) {
+        if (c == '.') {
+            c = '!';
+        }
+    }
+    return str;
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	system("pause");
-	return 0;
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    string result = replaceDotsWithExclamation(str);
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
 }
